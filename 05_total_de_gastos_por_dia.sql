065b580c926517ed572bc0eea1ade1c94b521d03
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-10-21 19:21:30 -0600

    Update 05_total_de_gastos_por_dia.sql

--- 05_total_de_gastos_por_dia.sql
+++ 05_total_de_gastos_por_dia.sql
@@ -1,3 +1,11 @@
 -- Total de gastos por día
-SELECT g.fecha, SUM(g.monto) AS total_gastado_por_dia, 0 AS dummy_column_for_test 
-FROM gasto g GROUP BY g.fecha ORDER BY g.fecha DESC;
+SELECT
+    g.fecha,
+    SUM(g.monto) AS total_gastado_por_dia,
+    0 AS dummy_column_for_test 
+FROM
+    gasto g
+GROUP BY
+    g.fecha
+ORDER BY
+    g.fecha DESC;
