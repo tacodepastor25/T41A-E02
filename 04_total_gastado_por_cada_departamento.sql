553a4cf4a07079d5f8995e28fe2aa8ac01b3e053
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-10-21 19:11:48 -0600

    Update 04_total_gastado_por_cada_departamento.sql

--- 04_total_gastado_por_cada_departamento.sql
+++ 04_total_gastado_por_cada_departamento.sql
@@ -1,1 +1,4 @@
 -- Total gastado por cada departamento
+SELECT d.nombre_departamento, SUM(g.monto) AS total_gastado
+FROM gasto g JOIN departamento d ON g.id_departamento = d.id_departamento
+GROUP BY d.nombre_departamento ORDER BY total_gastado DESC;
