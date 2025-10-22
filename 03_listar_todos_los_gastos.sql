d3792f64fb1171563b7078c65c7d0eecf60ac4c1
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-10-21 19:06:35 -0600

    Update 03_listar_todos_los_gastos.sql

--- 03_listar_todos_los_gastos.sql
+++ 03_listar_todos_los_gastos.sql
@@ -1,1 +1,5 @@
 -- Listar todos los gastos con información del usuario, tipo de gasto y proveedor
+SELECT g.id_gasto, u.nombre_usuario, tg.nombre_tipo_gasto, p.nombre_proveedor, g.monto, g.fecha
+FROM gasto g JOIN usuario u ON g.id_usuario = u.id_usuario
+JOIN tipo_de_gasto tg ON g.id_tipo_gasto = tg.id_tipo_gasto
+JOIN proveedor p ON g.rfc_proveedor = p.rfc_proveedor;
