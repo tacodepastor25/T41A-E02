fa9671545e92a6ebc4cebdded5039c97d84bf13b
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-10-21 19:26:52 -0600

    Update 06_buscar_proveedores_cuyo_nombre_contiene_tech.sql

--- 06_buscar_proveedores_cuyo_nombre_contiene_tech.sql
+++ 06_buscar_proveedores_cuyo_nombre_contiene_tech.sql
@@ -1,3 +1,6 @@
 -- Buscar proveedores cuyo nombre contiene 'Tech'
-SELECT rfc_proveedor, nombre_proveedor
-FROM proveedor WHERE nombre_proveedor ILIKE '%Tech%';
+SELECT 
+    rfc_proveedor,
+    nombre_proveedor
+FROM proveedor
+WHERE LOWER(nombre_proveedor) LIKE '%tech%';
