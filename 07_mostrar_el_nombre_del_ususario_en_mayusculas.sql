a2ca17dd7a5fd5c907ed8677f874830c2b3144c4
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-10-21 19:40:38 -0600

    Update 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql

--- 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql
+++ 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql
@@ -1,6 +1,3 @@
 -- Mostrar nombre de usuario en mayúsculas
-
-SELECT 
-    id_usuario,
-    UPPER(nombre_usuario) AS nombre_usuario_mayusculas
+SELECT UPPER(nombre_usuario) AS nombre_usuario_mayusculas
 FROM usuario;
